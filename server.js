2cbb0cbc7d163cacfabc7dbb0ccd63c2e4dced6b
Ben-Boyte <zissette1@gmail.com>
2022-07-20 09:50:02 -0500

    added connection.js and added more to server

--- server.js
+++ server.js
@@ -4,8 +4,32 @@ const express = require('express');
 const { notes } = require('./Develop/db/db.json');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
