867475bf28b8db159f1ce8801f503153862f3765
Ben-Boyte <zissette1@gmail.com>
2022-07-21 09:16:29 -0500

    added new files with a better template

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require('./Develop/db/db.json');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const PORT = process.env.PORT || 3001;
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(session(sess));
 
@@ -31,12 +28,12 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname + '/Develop/public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
