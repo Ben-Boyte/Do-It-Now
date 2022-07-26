600544c381080cf251042beaa33b1ddd923adf0e
Ben-Boyte <zissette1@gmail.com>
2022-07-26 14:09:12 -0500

    updated titles and functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
-const withAuth = (req, res, next) => {
+const auth = (req, res, next) => {
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
@@ -6,4 +6,4 @@ const withAuth = (req, res, next) => {
   }
 };
 
-module.exports = withAuth;
+module.exports = auth;
